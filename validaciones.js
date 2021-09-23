16a79d8731407f32df569c6426bf31b48c99430e
Luis Santiago Rodriguez Franco <srodriguez@integral.com.co>
2021-09-22 20:06:10 -0500

    Adición de los nombres de las funciones a validaciones.js

---
+++ validaciones.js
@@ -0,0 +1,22 @@
+
+// https://www.w3schools.com/jsref/jsref_match.asp 
+// con match se puede hacer la validacion de cadenas de texto
+// https://www.w3schools.com/js/js_regexp.asp
+// con test se puede hacer la validacion de patrones
+
+function validar_nombre_usuario(string) {
+    //implementacion
+}
+
+function validar_edad_usuario(edad) {
+    //implementacion
+}
+
+function validar_contrasena(string) {
+    //implementacion
+}
+
+
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
+module.exports.validar_edad_usuario = validar_edad_usuario;
+module.exports.validar_contrasena = validar_contrasena;
