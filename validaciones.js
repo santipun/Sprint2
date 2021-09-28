52e58bab21c12b95bcccc7a7f3611f9503a28869
OsorioLuis <91294233+OsorioLuis@users.noreply.github.com>
2021-09-27 20:43:38 -0500

    Creación de función validar_nombre_usuario

--- validaciones.js
+++ validaciones.js
@@ -4,8 +4,32 @@
 // https://www.w3schools.com/js/js_regexp.asp
 // con test se puede hacer la validacion de patrones
 
-function validar_nombre_usuario(string) {
-    //implementacion
+/*===================LEER PARA COMPRENSION DE LA FUNCION VALIDAR_NOMBRE_USUARIO===================
+Expresión regular:
+-retorna 0 cuando hay coincidencias exactas con las restricciones
+-retorna -1 cuando no hay coincidencias
+
+-retorna otros valores numericos cuando hay n cantidad de coincidencias. Por esta razón para tener en cuenta esos casos los condicionales
+usan el operador != -1, se tiene en cuenta solo los casos erroneos*/
+
+function validar_nombre_usuario(name){
+    if(name.startsWith(" ") == false && name.endsWith(" ") == false){  //verifica si el texto no inicia ni finaliza con espacios
+        if(name.search(/[^0-9]/) == 0){  //verifica si no hay numeros en la cadena
+            if(name.search(/ /) != -1){  //verifica si hay espacios en la cadena
+                if(name.search(/ [A-Z]/) != -1){ //verifica si hay espacios y el caracter a continuacion es mayuscula
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return name.startsWith(name[0].toUpperCase());
+            }
+        }else{
+            return false;
+        }
+    }else{
+        return false;
+    }
 }
 
 function validar_edad_usuario(edad){
