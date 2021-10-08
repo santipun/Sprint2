7690395e2e20d701593d3a60cb412cc7cae47ecf
Luis Puerto <luismiguel.puerto@hotmail.com>
2021-10-08 01:44:34 -0500

    Se agrega la funcion OrdenarArreglo();

---
+++ validacionesSprint3.js
@@ -0,0 +1,25 @@
+let registros = [];
+
+//Funcion realizada por Marcela
+function agregarRegistro(){
+    let usuario = document.getElementById('dato_nombre_usuario');
+    let edad = document.getElementById('dato_edad_usuario');
+    let contrasena = document.getElementById('dato_contrasena');
+    let datos = new Object(usuario, edad, contrasena);
+    registros.push(datos);
+}
+
+
+function OrdenarArreglo(registros){
+    //implementacion
+    
+    const ordenarPorEdad = (a,b)=>datos.edad - datos.edad;
+    registros.sort(ordenarPorEdad);
+
+    for(let i=0; i<registros.length; i++){
+    console.log(registros[i]);
+}
+
+module.exports.registros = registros;
+module.exports.OrdenarArreglo = OrdenarArreglo;
+module.exports.agregarRegistro = agregarRegistro;
